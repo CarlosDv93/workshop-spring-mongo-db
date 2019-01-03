02c819412cf2f0adbedc3fdec7365fc727ad4866
Carlos David A Arcos <carlos9_cv@hotmail.com>
2019-01-03 14:50:27 -0200

    Operação de instanciação da base de dados

---
+++ src/main/java/com/carlosdv93/udemy/workshopmongo/config/Instantiation.java
@@ -0,0 +1,29 @@
+package com.carlosdv93.udemy.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.carlosdv93.udemy.workshopmongo.domain.User;
+import com.carlosdv93.udemy.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.save(Arrays.asList(maria, alex, bob));
+	}
+
+}
