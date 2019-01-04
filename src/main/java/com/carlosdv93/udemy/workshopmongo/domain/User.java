2c151abcf6ea5f00dda688336f00e3e1539bc051
Carlos David A Arcos <carlos9_cv@hotmail.com>
2019-01-04 10:22:13 -0200

    Referenciando os posts do usuário

--- src/main/java/com/carlosdv93/udemy/workshopmongo/domain/User.java
+++ src/main/java/com/carlosdv93/udemy/workshopmongo/domain/User.java
@@ -1,8 +1,11 @@
 package com.carlosdv93.udemy.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 //@Document(collection="user") <-- pode ser usado assim tbm
@@ -14,6 +17,9 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy = true) //Lazy = true não carrega os posts automaticamente a lista de post. Só quando forem acessados diretamente.
+	private List<Post> posts = new ArrayList<>();
 	
 	public User() {
 		
@@ -48,6 +54,14 @@ public class User implements Serializable {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	@Override
